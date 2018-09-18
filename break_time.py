1f4919114acc8bdb7b5a6ba470e2b55c772b4ef4
hanyslmm <hanyslmm@gmail.com>
2018-09-18 03:06:51 +0200

    add the time and loop

--- break_time.py
+++ break_time.py
@@ -1,2 +1,11 @@
 import webbrowser
-webbrowser.open("https://www.youtube.com/watch?v=OrQNzqB35O4")
+import time
+
+total_breaks = input("how many breaks you need")
+break_count = 0
+
+print("this program started on "+time.ctime())
+while(break_count < total_breaks):
+    time.sleep(1*60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=OrQNzqB35O4")
+    break_count += 1
